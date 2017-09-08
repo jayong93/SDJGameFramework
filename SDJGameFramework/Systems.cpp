89306c4a598134429e024f087c30456b2996357b
최재용 <jayong93@gmail.com>
2017-09-08 22:53:36 +0900

    enable to render hierarchy model

--- SDJGameFramework/Systems.cpp
+++ SDJGameFramework/Systems.cpp
@@ -15,32 +15,41 @@ void RenderSystem::Render()
 	glClearColor(0.f, 0.f, 0.f, 1.f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 	for (auto& s : shapesList)
 	{
-		Object* owner = OM.Get(s.owner);
 		glPushMatrix();
+		Object* ancester = OM.Get(s.owner);
+		float mat[16];
+		while (ancester)
 		{
-			glTranslatef(owner->position.x, owner->position.y, owner->position.z);
-			glColor3fv(s.color);
-			switch (s.shapeType)
-			{
-				case Shape::CUBE:
-					glutWireCube(s.cube.size);
-					break;
-				case Shape::SPHERE:
-					glutWireSphere(s.sphere.radius, s.sphere.slice, s.sphere.stack);
-					break;
-				case Shape::CONE:
-					glutWireCone(s.cone.base, s.cone.height, s.cone.slice, s.cone.stack);
-					break;
-				case Shape::TORUS:
-					glutWireTorus(s.torus.innerRadius, s.torus.outerRadius, s.torus.side, s.torus.ring);
-					break;
-				case Shape::TEAPOT:
-					glutWireTeapot(s.teapot.size);
-					break;
-			}
+			glGetFloatv(GL_MODELVIEW_MATRIX, mat);
+			glLoadIdentity();
+			glTranslatef(ancester->position.x, ancester->position.y, ancester->position.z);
+			glMultMatrixf(mat);
+			ancester = OM.Get(ancester->parent);
 		}
+
+		glColor3fv(s.color);
+		switch (s.shapeType)
+		{
+			case Shape::CUBE:
+				glutWireCube(s.cube.size);
+				break;
+			case Shape::SPHERE:
+				glutWireSphere(s.sphere.radius, s.sphere.slice, s.sphere.stack);
+				break;
+			case Shape::CONE:
+				glutWireCone(s.cone.base, s.cone.height, s.cone.slice, s.cone.stack);
+				break;
+			case Shape::TORUS:
+				glutWireTorus(s.torus.innerRadius, s.torus.outerRadius, s.torus.side, s.torus.ring);
+				break;
+			case Shape::TEAPOT:
+				glutWireTeapot(s.teapot.size);
+				break;
+		}
+
 		glPopMatrix();
 	}
 	glutSwapBuffers();
