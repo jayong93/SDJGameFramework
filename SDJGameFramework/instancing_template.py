ac4db4494981b3dcb9296e04c030bc0210ff989c
최재용 <jayong93@gmail.com>
2017-09-13 23:41:16 +0900

    change build automation setting

--- SDJGameFramework/instancing_template.py
+++ SDJGameFramework/instancing_template.py
@@ -1,24 +1,24 @@
 from mako.template import Template
 import io
 import json
-import sys
-import os.path
-
-args = sys.argv
-
-if len(args) <= 1:
-    print("not enough arguments")
-    exit()
 
 data = None
 with io.open("template\\Components.json") as d:
     data = json.loads(d.read())
 
-tmp = Template(filename=str(args[1]), output_encoding='utf-8')
-input_name = os.path.basename(str(args[1]))
-output_name = os.path.splitext(input_name)[0]
-result = tmp.render(data=data)
+header_tmp = Template(filename="template\\Components.h.in", output_encoding='utf-8')
+body_tmp = Template(filename="template\\Components.cpp.in", output_encoding='utf-8')
 
-with io.open(output_name, "wb") as header:
+print("make Components.h")
+with io.open("Components.h", "wb") as header:
+    result = header_tmp.render(data=data)
     header.write(result)
-    print("Complete Instancing", input_name, "to", output_name)
+    print("Complete")
+
+print("------------------------------------------------")
+
+print("make Components.cpp")
+with io.open("Components.cpp", "wb") as body:
+    result = body_tmp.render(data=data)
+    body.write(result)
+    print("Complete")
